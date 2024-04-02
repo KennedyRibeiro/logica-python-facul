402419b557df2df5592413101fc97444c4a3bfa0
KennedyRibeiro <kennedydeveloper1@gmail.com>
2024-04-02 11:03:10 -0300

    começando o exercício de n° 10

--- Ex-python-lista2/prog10.py
+++ Ex-python-lista2/prog10.py
@@ -1,3 +1,17 @@
 #Construa um algoritmo que seja capaz de construir qual dentre os seguintes animais foi escolhido, através de perguntas e respostas. Animais possíveis: leão, cavalo, homem, macaco, morcego, baleia, avestruz, pinguim, pato, águia, tartaruga, crocodilo e cobra.#
 
 
+
+print("============================================================")
+print("==================Bem-Vindo ao Zoológico====================")
+print("============================================================")
+opcao = int(input("Escolha uma opção para chegar até o animal. Opção 1, 2 ou 3. "))
+print("============================================================")
+
+if opcao == 1:
+    print("Mamiferos.")
+    opcao = int(input("Agora para proseguir, escolha uma opção de 1 à 4. "))
+else:
+    if opcao >= 1 and opcao <= 4:
+        print("")
+    
