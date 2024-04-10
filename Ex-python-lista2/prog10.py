eb0b7ef46ebe7de69386233fcc5d48633b8596e3
KennedyRibeiro <kennedydeveloper1@gmail.com>
2024-04-10 15:00:28 -0300

    Lista 2 finalizada

--- Ex-python-lista2/prog10.py
+++ Ex-python-lista2/prog10.py
@@ -6,30 +6,30 @@ print("============================================================")
 print("==================Bem-Vindo ao Zoológico====================")
 print("============================================================")
 
-code = int(input("1-Mamíferos, 2-Aves, 3-Répteis"))
+code = int(input("1-Mamíferos, 2-Aves, 3-Répteis: "))
 
 if code == 1:
-    code = int(input("1-Quadrúpedes, 2-Bípedes, 3-Voadores, 4-Aquáticos"))
+    code = int(input("1-Quadrúpedes, 2-Bípedes, 3-Voadores, 4-Aquáticos: "))
     if code == 1:
-        code = int(input("1-Carnívoros, 2-Herbívoros"))
+        code = int(input("1-Carnívoros, 2-Herbívoros: "))
         if code == 1:
             print("O Animal é um leão")
         else:
             print("O Animal é um cavalo")
     elif code == 2:
-        code = int(input("1-Onívoros, 2-Frutiferos"))
+        code = int(input("1-Onívoros, 2-Frutiferos: "))
         if code == 1:
             print("O Animal é um Homem")
         else:
             print("O Animal é um Macaco")
     elif code == 3:
         print("O Animal é um Morcego")
-    else:
+    elif code == 4:
         print("O Animal é uma Baleia")
 elif code == 2:
-    code = int(input("1-Não Voadores, 2-Nadadores, 3-De rapina"))
+    code = int(input("1-Não Voadores, 2-Nadadores, 3-De rapina: "))
     if code == 1:
-        code = int(input("1-Tropical, 2-Polar"))
+        code = int(input("1-Tropical, 2-Polar: "))
         if code == 1:
             print("O Animal é um Avestruz")
         else:
@@ -39,12 +39,12 @@ elif code == 2:
     else:
         print("O Animal é uma Águia")
 elif code == 3:
-    code = int(input("1-Com casco, 2-Carnívoros, 3-Sem Patas"))
+    code = int(input("1-Com casco, 2-Carnívoros, 3-Sem Patas: "))
     if code == 1:
         print("O Animal é uma Tartaruga")
     elif code == 2:
         print("O Animal é um Crocodilo")
-    else:
+    elif code == 3:
         print("O Animal é uma Cobra")
                 
 
