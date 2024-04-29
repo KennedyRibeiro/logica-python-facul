fb632572f6e9b91e60d1a7beff8c596536a2d9fc
KennedyRibeiro <kennedydeveloper1@gmail.com>
2024-04-28 21:13:32 -0300

    fazendo ex 4 e 5 da lista 3

---
+++ Ex-python-lista3/prog4.py
@@ -0,0 +1,14 @@
+h = 0 
+num = 1
+
+cont = 1
+
+while cont <= 3:
+    den = int(input("Qual o valor do denominador? "))
+    h = num / den
+    
+    print("O valor de H é: ", h)
+    
+    cont += 1
+    
+
---
+++ Ex-python-lista3/prog5.py
@@ -0,0 +1,10 @@
+h = 0
+num = 1
+den = 1
+
+while den <= 50:
+    h = num / den
+    num = num + 2
+    den = den + 1
+
+print("O valor de H é: ", h)
