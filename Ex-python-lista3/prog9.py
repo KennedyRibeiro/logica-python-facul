7fa3c99848972e4d8234dcbf500cfa407e96235c
KennedyRibeiro <kennedydeveloper1@gmail.com>
2024-04-29 21:44:28 -0300

    Feito o EX 9 n° primo

--- Ex-python-lista3/prog9.py
+++ Ex-python-lista3/prog9.py
@@ -1,6 +1,19 @@
 # Construa um algoritmo que verifique se um número fornecido pelo usuário é primo ou não.
 
+cont = 2
+
 n = int(input("Digite um número para ser se ele é primo: "))
+
+while cont < n:
+    if n % cont != 1:
+        print("O número não é primo")
+        break
+    elif n % cont == 1:
+        print("O número é primo!")
+        break
+    cont =+ 1
+
+
 
 
         
