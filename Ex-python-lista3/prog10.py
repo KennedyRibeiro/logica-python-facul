b4ccaa87a11a5e5fd51a044fa4671dc020a43c98
KennedyRibeiro <kennedydeveloper1@gmail.com>
2024-05-06 19:21:06 -0300

    resolvendo o Ex 10

---
+++ Ex-python-lista3/prog10.py
@@ -0,0 +1,4 @@
+# Elabore um algoritmo que imprima todos os números primos existentes entre N1 e N2, em que N1 e N2 são números naturais fornecidos pelo usuário.
+
+n1 = int(input("Qual o primeiro número ?"))
+n2 = int(input("Qual o segundo número ?"))
