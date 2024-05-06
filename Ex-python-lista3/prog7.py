73e1d33fdc818ca4944add047b352c10cdb9ce82
KennedyRibeiro <kennedydeveloper1@gmail.com>
2024-05-06 20:13:34 -0300

    Ex 7 feito

--- Ex-python-lista3/prog7.py
+++ Ex-python-lista3/prog7.py
@@ -1,2 +1,12 @@
 # Elabore um algoritmo que calcule N! (fatorial de N), sendo que o valor inteiro de N é fornecido pelo usuário. Sabendo que
 
+numero = int(input("Digite o número: "))
+
+fat = 1
+cont = 2
+
+while cont <= numero:
+    fat = fat * cont
+    cont = cont + 1
+
+print(fat)
