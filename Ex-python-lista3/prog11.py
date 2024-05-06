bee0373f85204d51e9f6b66b2c67fd05da3a26c4
KennedyRibeiro <kennedydeveloper1@gmail.com>
2024-05-06 20:42:50 -0300

    Iniciado o programa 11

---
+++ Ex-python-lista3/prog11.py
@@ -0,0 +1,2 @@
+# Anacleto tem 1,50 metro e cresce 2 centímetros por ano, enquanto Felisberto tem 1,10 metro e cresce 3centímetros por ano. Construa um algoritmo que calcule e imprima quantos anos serão necessários para que Felisberto seja maior que Anacleto.
+
