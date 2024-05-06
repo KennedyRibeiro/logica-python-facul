befe22ee2018b662890bbfb8d264007f3e24430d
KennedyRibeiro <kennedydeveloper1@gmail.com>
2024-05-06 20:02:17 -0300

    Ex 10 feito

--- Ex-python-lista3/prog10.py
+++ Ex-python-lista3/prog10.py
@@ -2,3 +2,16 @@
 
 n1 = int(input("Qual o primeiro número ?"))
 n2 = int(input("Qual o segundo número ?"))
+
+while n1 <= n2:
+    primo = 0
+    metade = n1 // 2
+    cont = 2
+    while cont <= metade and primo == 0:
+        if n1 % cont == 0:
+            primo = 1
+        cont =+ 1
+    if primo == 0:
+        print(n1)
+    n1 =+ 1
+            
