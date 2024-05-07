fd84dc2ce8fa6ed108580a4c29cefe1ea520773e
KennedyRibeiro <kennedydeveloper1@gmail.com>
2024-05-07 15:19:46 -0300

    Ex eleição finalizado

--- provas/Eleicao-06-05.py
+++ provas/Eleicao-06-05.py
@@ -15,7 +15,39 @@ while voto > 0:
     elif voto == 3:
         print("Você votou no candidato número 3!")
         ThirdCandidato = ThirdCandidato + 1
+    elif voto != 1 and voto != 2 and voto != 3:
+        print("Você não pode votar nulo ou em branco! Somente em um dos candidatos: 1, 2 ou 3")
         
     print(FirtCandidato, SecondCandidato, ThirdCandidato)
 
-    
+totalVotos = FirtCandidato + SecondCandidato + ThirdCandidato
+
+percentual1 = (totalVotos * FirtCandidato) / 100
+percentual2 = (totalVotos * SecondCandidato) / 100
+percentual3 = (totalVotos * ThirdCandidato) / 100
+
+print("O Percentual de votos de cada candidato: ")
+print("Candidato 1: ",percentual1,"%")
+print("Candidato 2: ",percentual2,"%")
+print("Candidato 3: ",percentual3,"%")
+
+
+if FirtCandidato > SecondCandidato and FirtCandidato > ThirdCandidato:
+    print("O Candidato vencedor é o 1°!!")
+elif SecondCandidato > FirtCandidato and SecondCandidato > ThirdCandidato:
+    print("O Candidato vencedor é o 2°!!")
+elif ThirdCandidato > FirtCandidato and ThirdCandidato > SecondCandidato:
+    print("O Candidato vencedor é o 3°!!")
+
+
+if FirtCandidato == SecondCandidato:
+    print("O 1° candidato empatou com o 2°!!")
+elif SecondCandidato == ThirdCandidato:
+    print("O 2° candidato empatou com o 3°!!")
+elif FirtCandidato == ThirdCandidato:
+    print("O 1° candidato empatou com o 3°!!")
+
+
+
+
+
