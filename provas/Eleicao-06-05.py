4f02ee7af15f2ccfbe8516babfe59d3f5c8961de
KennedyRibeiro <kennedydeveloper1@gmail.com>
2024-05-07 11:13:09 -0300

    Iniciado Ex, algoritmo de votos

---
+++ provas/Eleicao-06-05.py
@@ -0,0 +1,13 @@
+FirtCandidato = 0
+SecondCandidato = 0
+ThirdCandidato = 0
+
+
+while voto > 0:
+    voto = int(input("Vote em seu canditado: 1, 2, 3 ou 0 para sair. "))
+    if voto == 1:
+        print("Você votou no candidato número 1! ")
+        FirtCandidato =+ 1
+    elif voto == 2:
+        print("Você votou no candidato número 1! ")
+        SecondCandidato =+ 1
