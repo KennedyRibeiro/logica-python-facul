56ff65b86f0b3b77d592a902e17a109c4d75d1c7
KennedyRibeiro <kennedydeveloper1@gmail.com>
2024-05-13 09:31:49 -0300

    fazendo ex 13 da Lista3

---
+++ Ex-python-lista3/pro13.py
@@ -0,0 +1,27 @@
+# Em uma eleição presidencial, existem quatro candidatos. Os votos são informados através de código. Os dados utilizados para a escrutinagem obedecem à seguinte codificação:
+
+canditado1 = 0
+canditado2 = 0
+canditado3 = 0
+canditado4 = 0
+nulo = 0
+branco = 0
+
+voto = 999
+
+while voto > 0:
+    voto = int(input("Seu voto vai para qual candidato ? 1, 2, 3, branco6 ou nulo5? "))
+    if voto == 1:
+        canditado1 += 1
+    elif voto == 2:
+        canditado2 =+ 1
+    elif voto == 3:
+        canditado3 += 1
+    elif voto == 4:
+        canditado4 += 1
+        
+
+
+
+
+
