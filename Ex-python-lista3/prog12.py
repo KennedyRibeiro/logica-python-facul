019c8ec32c106de7b21bd4b32b1706b2f7255c27
KennedyRibeiro <kennedydeveloper1@gmail.com>
2024-05-13 17:19:23 -0300

    EX 14  lista3

--- Ex-python-lista3/prog12.py
+++ Ex-python-lista3/prog12.py
@@ -1,3 +1,14 @@
 # Uma rainha requisitou os serviços de um monge e disse-lhe que pagaria qualquer preço. O monge, necessitando de alimentos, perguntou à rainha se o pagamento poderia ser feito com grãos de trigo dispostos em um tabuleiro de xadrez, de tal forma que o primeiro quadro contivesse apenas um grão e os quadros subsequentes, o dobro do quadro anterior. A rainha considerou o pagamento barato e pediu que o serviço fosse executado, sem se dar conta de que seria impossível efetuar o pagamento. Faça um algoritmo para calcular o número de grãos que o monge esperava receber.
 
 
+graos = 0
+quadro = 1
+cont = 1
+
+while cont <= 64:
+    graos = graos + quadro
+    quadro = quadro * 2
+
+    cont += 1
+
+print(graos)
--- Ex-python-lista3/prog14.py
+++ Ex-python-lista3/prog14.py
@@ -8,6 +8,9 @@ cont = 0
 menorAltF = 0
 menorAltM = 0
 
+maiorAltF = 0
+maiorAltM = 0
+
 contadorM = 0
 contadorF = 0
 
@@ -16,18 +19,34 @@ while cont <= 50:
     if sexo == "F":
         alt_f = input("Qual a sua altura ? ")
         f = alt_f
-        menorAlt = alt_f
-        if menorAlt < alt_f:
-            menorAlt = alt_f
+        menorAltF = alt_f
+        if alt_f < menorAltF:
+            menorAltF = alt_f
+        else:
+            maiorAltF = alt_f
+        
         contadorF += 1
     elif sexo == "M":
         alt_m = input("Qual a sua altura ? ")
         m = alt_m
         menorAltM = alt_m
-        if menorAltM < alt_m:
+        if alt_f < menorAltM:
             menorAltM = alt_m
+        else:  
+            maiorAltM = alt_m
+
         contadorM += 1
     
     cont += 1
+
+if menorAltF < menorAltM:
+    print("A menor altura do grupo é: ", menorAltF)
+else:
+    print("A menor altura do grupo é: ", menorAltM)
+
+if maiorAltF > maiorAltM:
+    print("A maior altura do grupo é: ", maiorAltF)
+else:
+    print("A maior altura do grupo é: ", maiorAltM)
 
 print("O total de homens é: ",contadorM)
