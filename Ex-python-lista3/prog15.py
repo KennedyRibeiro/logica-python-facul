f2b067aab9c286f710b83cd83c6db7142bc2a5a8
KennedyRibeiro <kennedydeveloper1@gmail.com>
2024-05-13 17:33:17 -0300

    EX 15, lista 3

---
+++ Ex-python-lista3/prog15.py
@@ -0,0 +1,2 @@
+# Calcule o imposto de renda de um grupo de 10 contribuintes, considerando que os dados de cada contribuinte,número do CPF, número de dependentes e renda mensal são valores fornecidos pelo usuário. Para cadacontribuinte será feito um desconto de 5% do salário mínimo por dependente. Os valores da alíquota para cálculodo imposto são:
+
