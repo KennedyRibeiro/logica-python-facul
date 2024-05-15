d8467f1fe239def3cf19b3458ac1fe336cf64a6e
KennedyRibeiro <kennedydeveloper1@gmail.com>
2024-05-15 20:10:13 -0300

    começando a lista4. Vetores e listas

---
+++ Ex-python-lista4/notas.py
@@ -0,0 +1,31 @@
+
+n = 10
+
+notas = [""] * n
+i = 0 
+
+while i < n:
+    notas[i] = float(input("Nota: "))
+
+    i += 1
+
+media = 0
+i = 0
+
+while i < n:
+    media += notas [i]
+
+    i += 1
+
+media = media / n
+acima_media = 0
+
+while i < n:
+    if notas[i] > media:
+        acima_media += 1
+    
+    i += 1
+
+print(media, acima_media)
+
+
