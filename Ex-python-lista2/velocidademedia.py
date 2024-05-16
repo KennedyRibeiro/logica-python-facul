b554fd78ff04de8bf65591e6f8a6ac14e9e17f70
KennedyRibeiro <kennedydeveloper1@gmail.com>
2024-05-16 15:40:07 -0300

    Ex feito

---
+++ Ex-python-lista2/velocidademedia.py
@@ -0,0 +1,7 @@
+
+velocidade = float(input("Qual a velocidade média do carro? "))
+distancia = float(input("Qual é a distância em km do destino? "))
+
+tempo = distancia / velocidade
+
+print("Você chegará no local em: ", tempo)
