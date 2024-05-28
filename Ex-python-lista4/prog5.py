ea0580b096098b73acc328183a411a07562f4da6
KennedyRibeiro <kennedydeveloper1@gmail.com>
2024-05-28 10:23:14 -0300

    Feito ex5 lista4

--- Ex-python-lista4/prog5.py
+++ Ex-python-lista4/prog5.py
@@ -1,4 +1,4 @@
-
+#  Faça um algoritmo que receba as notas da primeira prova de N alunos e armazene essas notas em um vetor. Receba as notas da segunda prova de N alunos e armazene essas notas em um outro vetor. Calcule e imprima a média entre essas duas notas de cada aluno.
 
 alunos = int(input("Quantos alunos são? "))
 notas1 = [""] * alunos
@@ -8,11 +8,6 @@ i = 0
 
 while i < alunos:
     notas1[i] = int(input("Aluno insira sua nota da prova 1: "))
-    i += 1
-
-i = 0
-
-while i < alunos:
     notas2[i]= int(input("Aluno insira sua nota da prova 2: "))
     i += 1
 
@@ -20,11 +15,12 @@ i = 0
 
 while i < alunos:
     media = (notas1[i] + notas2[i]) / 2
-    print(i, media)
-    i += 1
 
-
-
+    if media > 70:
+        print(i, media, "Aprovado!")
+    elif media < 50 and media > 30:
+        print(i, media, "Em exame!")
+    elif media < 30:
+        print(i, media, "Reprovado!")
     
-
-
+    i += 1
