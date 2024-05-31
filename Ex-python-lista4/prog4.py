e9d94c9a75b7f96d9d7f070041dc24419f012eba
KennedyRibeiro <kennedydeveloper1@gmail.com>
2024-05-31 12:09:52 -0300

    ex4 lista4 feito

--- Ex-python-lista4/prog4.py
+++ Ex-python-lista4/prog4.py
@@ -1,26 +1,27 @@
 # Faça um algoritmo que receba a temperatura média de cada mês do ano e armazene essas temperaturas em um vetor, calcule e imprima a maior e a menor temperatura do ano em que mês estas temperaturas aconteceram.
 
-tempo = 3
+tempo = 6
 temp = [""] * tempo
 
 i = 0
 
 while i < tempo:
-    temp[i] = float(input("Qual é temperatura média desse mês ?"))
+    temp[i] = int(input("Qual é temperatura média desse mês ?"))
     
     i += 1
 
 i = 0
+maior = 0
+menor = 999
 
 while i < tempo:
-    maior = temp[i]
-    menor = 999
-    if temp[i] >= maior:
-        maior = temp[i]
     
+    if temp[i] > maior:
+        maior = temp[i]
+    elif temp[i] < menor:
+        menor = temp[i]
     
     i += 1
 
-
-print("A maior temperatura é: ",maior)
-#print("A menor temperatura é: ",menor)
+print("A maior temperatura é: ",maior,"c")
+print("A menor temperatura é: ",menor,"c")
