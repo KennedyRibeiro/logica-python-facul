5deb441f669d4c85385451b7831cc7693e500bb2
KennedyRibeiro <kennedydeveloper1@gmail.com>
2024-06-19 17:15:40 -0300

    ex1 lista 5

--- Ex-python-lista5/prog1.py
+++ Ex-python-lista5/prog1.py
@@ -1,1 +1,18 @@
 # Desenvolva um algoritmo que após armazenar valores numéricos em uma matriz N x M, calcule e mostre o somatório desses valores.
+
+n = 4
+m = 2
+
+mat = [[""] * m for i in range (n)]
+
+i = 0 
+
+while i < n:
+    j = 0
+    while j < m:
+        mat[i][j] = int(input(f"Digite o valor {i} | {j} : "))
+        j += 1
+
+    i += 1
+
+
