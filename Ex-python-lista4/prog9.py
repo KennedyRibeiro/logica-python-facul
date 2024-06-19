dd41b0d074fcea647c03985f92621abba3011310
KennedyRibeiro <kennedydeveloper1@gmail.com>
2024-06-19 11:44:04 -0300

    ex 9 lista4

---
+++ Ex-python-lista4/prog9.py
@@ -0,0 +1,8 @@
+# Desenvolva um algoritmo que leia um vetor de 20 posições inteiras e o coloque em ordem crescente, utilizando a seguinte estratégia de ordenação:
+
+n = 20
+
+vet = [""] * n
+
+i = 0
+
