2deede7abfb6251bc77c8ac27ecd3770a994ca3f
KennedyRibeiro <kennedydeveloper1@gmail.com>
2024-06-20 08:10:01 -0300

    ex1 lista5 feito

--- Ex-python-lista5/prog1.py
+++ Ex-python-lista5/prog1.py
@@ -1,18 +1,29 @@
-# Desenvolva um algoritmo que após armazenar valores numéricos em uma matriz N x M, calcule e mostre o somatório desses valores.
-
 n = 4
 m = 2
 
-mat = [[""] * m for i in range (n)]
+mat = [[""] * m for i in range(n)]
 
-i = 0 
+i = 0
 
 while i < n:
     j = 0
     while j < m:
-        mat[i][j] = int(input(f"Digite o valor {i} | {j} : "))
+        mat[i][j] = int(input(f"Valor ({i} : {j}):"))
+
         j += 1
 
     i += 1
 
+soma = 0
+i = 0
 
+while i < n:
+    j = 0
+    while j < m:
+        soma += mat[i][j]
+
+        j += 1
+
+    i += 1
+
+print(f"O valor da soma é :{soma}")
