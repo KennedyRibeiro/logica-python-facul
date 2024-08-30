960b2c16722fee39ba33df701c17b6f0da693fde
KennedyRibeiro <kennedydeveloper1@gmail.com>
2024-08-30 17:57:16 -0300

    prog4 finalizado

--- trabalho1_facul/prog4.py
+++ trabalho1_facul/prog4.py
@@ -1,14 +1,17 @@
-
+# Exibe a mensagem de boas-vindas com o nome do desenvolvedor
 print("-----------------------------------------")
 print("Bem-vindos à empresa do Kennedy Ribeiro")
 print("-----------------------------------------")
 
-
+# lista e variavel
 lista_funcionarios = []
 id_global = 4971058  
 
-
+# função de cadastrar o usuario
 def cadastrar_funcionario(id):
+    print("-----------------------------------------")
+    print("------- MENU CADASTRAR FUNCIONARIO ------")
+    print(id)
     nome = input("Por favor entre com o nome do funcionário: ")
     setor = input("Por favor entre com o setor do funcionário: ")
     salario = float(input("Por favor entre com o salário do funcionário: "))
@@ -23,38 +26,54 @@ def cadastrar_funcionario(id):
     lista_funcionarios.append(funcionario.copy())
     print(f"Funcionário {nome} cadastrado com sucesso!\n")
 
-
+# função para consultar usuario
 def consultar_funcionarios():
     while True:
-        print("\nConsultar Funcionários:")
-        print("1. Consultar Todos")
-        print("2. Consultar por Id")
-        print("3. Consultar por Setor")
-        print("4. Retornar ao menu")
-        opcao = int(input("Escolha uma opção: "))
+        print("-----------------------------------------")
+        print("------- MENU CONSULTAR FUNCIONARIO ------")
+        print("1 - Consultar Todos os funcionários")
+        print("2 - Consultar funcionário por Id")
+        print("3 - Consultar funcionário por Setor")
+        print("4 - Retornar ao menu")
+        opcao = int(input("Escolha a opção desejada: "))
 
         if opcao == 1:
             print("\nTodos os Funcionários:")
+            print("")
             for funcionario in lista_funcionarios:
-                print(funcionario)
+                print('ID: ',funcionario['id'])
+                print('Nome: ',funcionario['nome'])
+                print('Setor: ',funcionario['setor'])
+                print('Salário: R$',funcionario['salario'])
+                print("")
 
         elif opcao == 2:
             id_consulta = int(input("Digite o Id do funcionário: "))
             encontrado = False
             for funcionario in lista_funcionarios:
                 if funcionario['id'] == id_consulta:
-                    print(funcionario)
+                    print("----------------------")
+                    print('ID: ',funcionario['id'])
+                    print('Nome: ',funcionario['nome'])
+                    print('Setor: ',funcionario['setor'])
+                    print('Salário: R$',funcionario['salario'])
+                    print("")
                     encontrado = True
                     break
             if not encontrado:
                 print("Id não encontrado.")
 
         elif opcao == 3:
-            setor_consulta = input("Digite o setor: ")
+            setor_consulta = input("Digite o setor do(s) funcionário(s): ")
             encontrados = [funcionario for funcionario in lista_funcionarios if funcionario['setor'] == setor_consulta]
             if encontrados:
                 for funcionario in encontrados:
-                    print(funcionario)
+                    print("----------------------")
+                    print('ID: ',funcionario['id'])
+                    print('Nome: ',funcionario['nome'])
+                    print('Setor: ',funcionario['setor'])
+                    print('Salário: R$',funcionario['salario'])
+                    print("")
             else:
                 print("Nenhum funcionário encontrado neste setor.")
 
@@ -64,9 +83,11 @@ def consultar_funcionarios():
         else:
             print("Opção inválida.")
 
-# EXIGÊNCIA DE CÓDIGO 5 de 8
+# função para remover o usuario
 def remover_funcionario():
     while True:
+        print("-----------------------------------------")
+        print("------- MENU REMOVER FUNCIONARIO ------")
         id_remocao = int(input("Digite o Id do funcionário a ser removido: "))
         for funcionario in lista_funcionarios:
             if funcionario['id'] == id_remocao:
@@ -75,8 +96,9 @@ def remover_funcionario():
                 return
         print("Id inválido.")
 
-# EXIGÊNCIA DE CÓDIGO 6 de 8
+# loop do menu principal
 while True:
+    print("-----------------------------------------")
     print("------------- MENU PRINCIPAL ------------")
     print("1 - Cadastrar Funcionários")
     print("2 - Consultar Funcionário(s)")
